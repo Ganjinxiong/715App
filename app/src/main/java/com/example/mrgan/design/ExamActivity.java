d65eba970192d69fcbc42435cacd10fd704ebfbd
Ganjinxiong <1053591740@qq.com>
2019-03-21 18:26:45 +0800

    3.21.1

--- app/src/main/java/com/example/mrgan/design/ExamActivity.java
+++ app/src/main/java/com/example/mrgan/design/ExamActivity.java
@@ -194,6 +194,12 @@ public class ExamActivity extends AppCompatActivity {
                     }
                     trueCount++;
                 } else if (answer.equals("")) {
+                    Question question = new Question();
+                    question.setqString(questionStr);
+                    question.setGrade(grade);
+                    question.setResult(result);
+                    question.setAnswer("?");
+                    question.save();
                     if (wrong) {
                         disWrong();
                         wrong =false;
@@ -201,6 +207,12 @@ public class ExamActivity extends AppCompatActivity {
                 } else {
                     showWrong();
                     wrong =true;
+                    Question question = new Question();
+                    question.setqString(questionStr);
+                    question.setGrade(grade);
+                    question.setResult(result);
+                    question.setAnswer(answer);
+                    question.save();
                     vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
                     vibrator.vibrate(100);
                 }
@@ -219,6 +231,12 @@ public class ExamActivity extends AppCompatActivity {
         skipButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Question question = new Question();
+                question.setqString(questionStr);
+                question.setGrade(grade);
+                question.setResult(result);
+                question.setAnswer("?");
+                question.save();
                 if (totalCount == 25) {
                    showResult();
                 } else {
--- app/src/main/java/com/example/mrgan/design/RectifyActivity.java
+++ app/src/main/java/com/example/mrgan/design/RectifyActivity.java
@@ -1,8 +1,6 @@
 package com.example.mrgan.design;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
@@ -11,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -19,69 +16,67 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.litepal.LitePal;
+
 import java.lang.ref.WeakReference;
+import java.util.Iterator;
+import java.util.List;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
-//口算练习模块
-public class PracticeActivity extends AppCompatActivity {
+public class RectifyActivity extends AppCompatActivity {
 
     private Intent intent;
-    private String grade, type,question,questionStr,oldStr,result, answer;
-    private String[] questionItem;
-    private int maxLevel, minLevel, nowLevel, correct;
-    private TextView nowTextView, lastTextView, nextTextView;
+    private String grade, answer, result;
+    private ActionBar actionBar;
+    private Button exitButton, skipButton;
     private EditText answerEditText;
     private ImageView judgeImage;
-    private Button exitButton, skipButton;
-    private MyHandler handler;
+    private TextView oldquestionTextView, oldanswerTextView;
+    private Handler handler;
     private Message message;
-    private ActionBar actionBar;
-    private Boolean isfirst;
-    private QuizGive quizGive;
+    private List<Question> questionList,questionList2;
+    private Iterator iterator,iterator2;
 
-
-
-    @SuppressLint("SetTextI18n")
-    public void createQuestion(){
-        answerEditText.setText("");
-        answer = "xxx";
-        //是否第一次出题，是就出两次题，不显示上次的题目
-        if (isfirst) {
-            Log.d("aaa",grade+type+nowLevel);
-            quizGive = new QuizGive(grade,type,nowLevel);
-            question = quizGive.give();
-            questionItem = question.split("=");
-            questionStr = questionItem[0]+"=";
-            oldStr =questionStr;//错题用
-            nowTextView.setText(questionStr);
-            isfirst = false;
-        } else {
+    private void createQuestion() {
+        if (iterator.hasNext()) {
+            answerEditText.setText("");
             judgeImage.setImageResource(R.drawable.xie2);
-            lastTextView.setText(nowTextView.getText().toString() + result);
-            oldStr =questionStr;//错题用
-            nowTextView.setText(questionStr);
+            Question question = (Question) iterator.next();
+            oldquestionTextView.setText(question.getqString());
+            oldanswerTextView.setText(question.getAnswer());
+            result = question.getResult();
+        } else {
+            questionList = LitePal.where("grade like ?", grade).find(Question.class);
+            iterator = questionList.iterator();
+            if (iterator.hasNext()) {
+               createQuestion();
+            }else {
+                new SweetAlertDialog(RectifyActivity.this, SweetAlertDialog.SUCCESS_TYPE)
+                        .setTitleText("该年级的错题已纠正")
+                        .setConfirmText("退出纠正")
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog.dismissWithAnimation();
+                                finish();
+                            }
+                        })
+                        .show();
+            }
         }
-        //本题答案
-        result = questionItem[1];
-        //下一题
-        quizGive = new QuizGive(grade,type,nowLevel);
-        question = quizGive.give();
-        questionItem = question.split("=");
-        questionStr = questionItem[0]+"=";
-        nextTextView.setText(questionStr+"?");
     }
 
     static class MyHandler extends Handler {
-        WeakReference<PracticeActivity> mActivity;
+        WeakReference<RectifyActivity> mActivity;
 
-        MyHandler(PracticeActivity activity) {
-            mActivity = new WeakReference<PracticeActivity>(activity);
+        MyHandler(RectifyActivity activity) {
+            mActivity = new WeakReference<RectifyActivity>(activity);
         }
 
         @Override
         public void handleMessage(Message msg) {
-            PracticeActivity theActivity = mActivity.get();
+            RectifyActivity theActivity = mActivity.get();
             switch (msg.what) {
                 case 1:
                     SystemClock.sleep(1000);
@@ -96,41 +91,27 @@ public class PracticeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_practice);
+        setContentView(R.layout.activity_rectify);
 
-        judgeImage = findViewById(R.id.judge);
-        answerEditText = findViewById(R.id.answer);
-        exitButton = findViewById(R.id.exit);
-        skipButton = findViewById(R.id.skip);
-        nowTextView = findViewById(R.id.now);
-        lastTextView = findViewById(R.id.last);
-        nextTextView = findViewById(R.id.next);
-
-
-        //接收数据
         intent = getIntent();
         grade = intent.getStringExtra("grade");
-        type = intent.getStringExtra("type");
-        if (grade.equals("一年级"))
-            maxLevel = 4;
-        else
-            maxLevel = 2;
-        minLevel = 0;
-        nowLevel = minLevel;
-
-
 
         //隐藏标题栏
         actionBar = getSupportActionBar();
         if (actionBar != null)
             actionBar.hide();
 
-        //初始化题目
-        handler = new MyHandler(this);
-        isfirst = true;
-        correct = 0 ;
-        createQuestion();
+        exitButton = findViewById(R.id.exit3);
+        skipButton = findViewById(R.id.skip3);
+        answerEditText = findViewById(R.id.answer3);
+        judgeImage = findViewById(R.id.judge3);
+        oldquestionTextView = findViewById(R.id.oldquestion);
+        oldanswerTextView = findViewById(R.id.oldanswer);
 
+        handler = new MyHandler(this);
+        questionList = LitePal.where("grade like ?", grade).find(Question.class);
+        iterator = questionList.iterator();
+        createQuestion();
 
         //判断对错，进行下一题
         answerEditText.addTextChangedListener(new TextWatcher() {
@@ -150,11 +131,7 @@ public class PracticeActivity extends AppCompatActivity {
                     answer = s.toString();
                     if (answer.equals(result)) {
                         judgeImage.setImageResource(R.drawable.dui);
-                        correct ++;
-                        if (correct >=5 && nowLevel<maxLevel){
-                            correct = 0;
-                            nowLevel++;
-                        }
+                        LitePal.deleteAll(Question.class, "qString = ?", oldquestionTextView.getText().toString());
                         new Thread(new Runnable() {
                             @Override
                             public void run() {
@@ -170,22 +147,30 @@ public class PracticeActivity extends AppCompatActivity {
             }
         });
 
-
         //跳过这题
         skipButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                correct = 0 ;
-                if (nowLevel>minLevel){
-                    nowLevel--;
-                }
-                Question question = new Question();
-                question.setqString(oldStr);
-                question.setGrade(grade);
-                question.setResult(result);
-                question.setAnswer("?");
-                question.save();
+                questionList2 = LitePal.where("grade like ?", grade).find(Question.class);
+                iterator2 = questionList.iterator();
+                iterator2.next();
+                if (iterator2.hasNext())
                 createQuestion();
+                else {
+                    new SweetAlertDialog(RectifyActivity.this, SweetAlertDialog.SUCCESS_TYPE)
+                            .setTitleText("这是最后一题了")
+                            .setConfirmText("继续纠正")
+                            .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                @Override
+                                public void onClick(SweetAlertDialog sDialog) {
+                                    sDialog.dismissWithAnimation();
+                                    questionList = LitePal.where("grade like ?", grade).find(Question.class);
+                                    iterator = questionList.iterator();
+                                    createQuestion();
+                                }
+                            })
+                            .show();
+                }
             }
         });
 
@@ -193,10 +178,10 @@ public class PracticeActivity extends AppCompatActivity {
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new SweetAlertDialog(PracticeActivity.this, SweetAlertDialog.WARNING_TYPE)
-                        .setTitleText("要退出练习吗？")
-                        .setCancelText("是，退出练习")
-                        .setConfirmText("不，继续练习")
+                new SweetAlertDialog(RectifyActivity.this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("要退出错题纠正吗？")
+                        .setCancelText("是，退出纠正")
+                        .setConfirmText("不，继续纠正")
                         .showCancelButton(true)
                         .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
                             @Override
@@ -221,11 +206,11 @@ public class PracticeActivity extends AppCompatActivity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         //点击返回键调用方法
-        if(keyCode==KeyEvent.KEYCODE_BACK){
-            new SweetAlertDialog(PracticeActivity.this, SweetAlertDialog.WARNING_TYPE)
-                    .setTitleText("要退出练习吗？")
-                    .setCancelText("是，退出练习")
-                    .setConfirmText("不，继续练习")
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new SweetAlertDialog(RectifyActivity.this, SweetAlertDialog.WARNING_TYPE)
+                    .setTitleText("要退出错题纠正吗？")
+                    .setCancelText("是，退出纠正")
+                    .setConfirmText("不，继续纠正")
                     .showCancelButton(true)
                     .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
                         @Override
@@ -244,5 +229,4 @@ public class PracticeActivity extends AppCompatActivity {
         }
         return false;
     }
-
 }
